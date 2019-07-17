98cabfe2cdceb76d13cdb18427a8c506d51c363d
jordiascension <jordiascension@enterpriseconsulting.es>
2019-07-18 01:15:44 +0200

    First Commit, with example completed in .NET Core

---
+++ ReadConfiguration/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ReadConfiguration.Controllers
+{
+    public class HomeController : Controller
+    {
+        IConfiguration _configuration;
+        public HomeController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [Route("home/index")]
+        public IActionResult Index()
+        {
+            ViewBag.connectionstring = _configuration["ConnectionStrings:DefaultConnection"];
+            ViewBag.status = _configuration["status"];
+
+            return View();
+        }
+    }
+}
---
+++ ReadConfiguration/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ReadConfiguration
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; set; }
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+               .AddJsonFile("appSettings.json");
+            Configuration = builder.Build();
+        }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            //Second way to inject Configuration to controllers
+            services.AddSingleton<IConfiguration>(Configuration);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+            app.Run(context => {
+                var status = Configuration["status"];
+                var connectionString = Configuration["ConnectionStrings:DefaultConnection"];
+                context.Response.WriteAsync("Default Connection: " + connectionString);
+                context.Response.WriteAsync("<br/>");
+                return context.Response.WriteAsync("Status: " + status);
+            });
+        }
+    }
+}
